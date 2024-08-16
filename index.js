e3ccdaed3b6ddc8e5cb31a795d3ea94a8884d80d
830008 <nrollins189@gmail.com>
2024-08-15 20:41:45 -0500

    Make Discord buttons function

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3001;
 app.use(express.static('public'));
+app.get('/discord', (req, res) => {
+    res.redirect('https://discord.gg/akaribot')
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
