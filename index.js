72038db6c01ffc2c00fc0b4fe4984bb91b116d39
830008 <nrollins189@gmail.com>
2024-08-15 21:19:29 -0500

    Allow shorter urls and add status page

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const port = 3001;
 app.use(express.static('public'));
 app.get('/discord', (req, res) => {
     res.redirect('https://discord.gg/akaribot')
+});
+app.use((req, res, next) => {
+    const filePath = `${__dirname}/public${req.path}.html`;
+    if (fs.existsSync(filePath)) {
+        res.sendFile(filePath);
+    } else {
+        next();
+    }
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
